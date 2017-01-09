914f6de88c134b7f408d307a4d835948e2995775
capusta <gvozdev@gmail.com>
2017-01-08 21:31:46 -0800

    quick libraries for debug

--- tests/dbtests.js
+++ tests/dbtests.js
@@ -1,7 +1,10 @@
-console.log("starting golf app ... just for testing loki");
-// initialize loki
+global.debug = true; // can run various code in debug mode
+var logger = require('../lib/logger.js');
+logger.log("library loaded");
 var loki = require('lokijs');
-// looking good.  initializing db
+logger.log("loki db loaded");
+
+// Loki specific things:
 var options = {autosave: true}; // quick autosave ... cuuz .. fuuck
 var db = new loki('data/golf.json', options);
 
@@ -12,12 +15,15 @@ users.insert({name:'User two', hole: 7, score: 1});
 // db remains open, listening ... cuz ... autosave ... lets close it,
 // bring on the IO
 db.close();
-console.log("databse is closed");
+logger.log("databse is closed");
 
 var one = users.find( {'name':'User one'});
-console.log(one);
-console.log("this should have found something ");
+logger.log(one);
+logger.log("this should have found something ");
 
 var one_bad = users.find( {'name':'User onezzz'});
 // nope, did not find it.  we'll do should.js later on for testing
-console.log(one_bad + " wuut ... above line should be from line 19");
+logger.log(one_bad + " wuut ... above line should be from line 19");
+
+global.debug = false;
+logger.log("this line should not be displayed");
