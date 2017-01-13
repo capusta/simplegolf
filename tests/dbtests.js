43538544d0bde88aa20daed9209eba4afb7e202b
capusta <gvozdev@gmail.com>
2017-01-12 20:20:41 -0800

    Quick clean up of testing cases

--- tests/dbtests.js
+++ tests/dbtests.js
@@ -1,8 +1,8 @@
 global.debug = true; // can run various code in debug mode
 var logger = require('../lib/logger.js');
-logger.log("library loaded");
+logger.log("ok: library loaded");
 var loki = require('lokijs');
-logger.log("loki db loaded");
+logger.log("ok: loki db loaded");
 
 // Loki specific things:
 var options = {autosave: true}; // quick autosave ... cuuz .. fuuck
@@ -15,15 +15,15 @@ users.insert({name:'User two', hole: 7, score: 1});
 // db remains open, listening ... cuz ... autosave ... lets close it,
 // bring on the IO
 db.close();
-logger.log("databse is closed");
+logger.log("ok: databse is closed");
 
 var one = users.find( {'name':'User one'});
-logger.log(one);
-logger.log("this should have found something ");
+logger.log("ok" + JSON.stringify(one));
+logger.log("ok: above should have found something ");
 
 var one_bad = users.find( {'name':'User onezzz'});
 // nope, did not find it.  we'll do should.js later on for testing
-logger.log(one_bad + " wuut ... above line should be from line 19");
+logger.log(one_bad + "ok: this should be the only text");
 
 global.debug = false;
-logger.log("this line should not be displayed");
+logger.log("error: this line should not be displayed");
