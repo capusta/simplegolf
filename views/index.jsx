b691fb38f0e78ccacb998fa95c1429ca9c78e51b
capusta <gvozdev@gmail.com>
2017-01-17 22:58:13 -0800

    omg ... this too long to install on 512mb of ram

--- views/index.jsx
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-
-class HelloWorld extends React.Component {
-    render() {
-        return <div> Hello {this.props.thing}</div>;
-    }
-}
-
-module.exports = HelloWorld;
