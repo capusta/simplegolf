19e0ac0f67aef5b38aa65f030c464a980c9366cf
capusta <gvozdev@gmail.com>
2017-02-14 17:05:48 -0800

    Add a good and bad endpoint ... quick sanity checks

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,11 @@ const logger = require('../lib/logger')
 
 routes.get('/game/:gameid', (req, res) => {
     //todo: check if the appropriate game id has been logged
-    res.status(200).json({ "game_id": req.params.gameid, status: false});
+    if(req.params.gameid == 'bad'){
+        res.status(400).json({ error: true });
+    } else {
+        res.status(200).json({ game_id: req.params.gameid, error: false });
+    }
 });
 
 module.exports = routes;
