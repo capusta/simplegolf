f1edb58434c324f1da6de0400d521a7e28da3932
capusta <gvozdev@gmail.com>
2017-02-16 19:15:41 -0800

    Changing css

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,15 +1,15 @@
 import React, { PropTypes, Component } from 'react';
 import classnames from 'classnames';
-import './style.css';
 
 export default class Game extends Component {
 
   render() {
     const { className, ...props } = this.props;
+    var players = this.props.players
     return (
       <div className={classnames('Game', className)} {...props}>
-        <h3> Game component </h3>
         <h5> {this.props.gameName} </h5>
+        <p>{players}.</p>
       </div>
     );
   }
