facbbcffa6ce48970ca95feda6dcfc6e843e2ad0
capusta <gvozdev@gmail.com>
2017-02-20 20:55:36 -0800

    QUick change for tests

--- tests/dbtests.js
+++ tests/dbtests.js
@@ -9,19 +9,19 @@ var options = {autosave: true}; // quick autosave ... cuuz .. fuuck
 var db = new loki('data/golf.json', options);
 
 // I guess we'll start with users
-var users = db.addCollection('users');
-users.insert({name:'User one', hole: 8, score: 2});
-users.insert({name:'User two', hole: 7, score: 1});
+var players = db.addCollection('players');
+players.insert({name:'User one', hole: 8, score: 2});
+players.insert({name:'User two', hole: 7, score: 1});
 // db remains open, listening ... cuz ... autosave ... lets close it,
 // bring on the IO
 db.close();
 logger.log("ok: databse is closed");
 
-var one = users.find( {'name':'User one'});
+var one = players.find( {'name':'User one'});
 logger.log("ok" + JSON.stringify(one));
 logger.log("ok: above should have found something ");
 
-var one_bad = users.find( {'name':'User onezzz'});
+var one_bad = players.find( {'name':'User onezzz'});
 // nope, did not find it.  we'll do should.js later on for testing
 logger.log(one_bad + "ok: this should be the only text");
 
