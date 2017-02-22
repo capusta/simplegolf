249363aef4b27cbff459e2e020d77c145b49dd3a
capusta <gvozdev@gmail.com>
2017-02-21 16:26:20 -0800

    Game Component has to do more

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -2,31 +2,78 @@ import React, { PropTypes, Component } from 'react';
 import classnames from 'classnames';
 
 export default class Game extends Component {
+    //Props: gameName, setAlert
     constructor(props){
         super(props)
         this.state = {
-            players: [],
-            holes: [],
-            scores: [],
-            currentPlayer: null,
-            currentHole:   null,
-            currentScore:  null,
+            gameName: null,
+            players:  [],
+            activePlayer: null,
+            activeHole:   null,
         }
+        this.handleGameLoad = this.handleGameLoad.bind(this);
+        this.handleGameName = this.handleGameName.bind(this);
+        //this.getPlayers = this.getPlayers.bind(this);
+    }
+    handleGameName(event){
+        this.setState({userGameInput: event.target.value})
+    }
+    handleGameLoad(event){
+        var that = this;
+        event.preventDefault();
+        this.props.setAlert("Loading game " +  this.props.gameName)
+        fetch(process.env.REACT_APP_BASE_URL+'/api/games/'+this.state.userGameInput)
+            .then(function(res){
+                if(res.status != 200){
+                    return {gameName: null}
+                } else {
+                    return res.json();
+                }
+            })
+            .then(function(data){
+                if (data.gameName == null) {
+                    that.props.setAlert("Error parsing game data for  " + data.gameName)
+                } else {
+                    // Update alert banner
+                    that.props.setAlert("game loaded")
+                    // Update parent display
+                    that.props.setGameName(data.gameName);
+                    // Update local state
+                    that.setState({gameName: data.gameName});
+                    // Grab initial player data
+                    that.getPlayers(data.gameName)
+                }
+            })
+    }
+    getPlayers(gameID){
+        var that = this;
+        fetch(process.env.REACT_APP_BASE_URL+'/api/players/'+gameID)
+            .then(function(res){
+                return res.json();
+            })
+            .then(function(data){
+                console.log('fetching players: ' + JSON.stringify(data))
+                that.setState({players: data})
+            })
     }
     render() {
         const { className, ...props } = this.props;
-        console.log("rendering game " + JSON.stringify(this.props))
-        if (this.props.gameName == null){
+        var numPlayers = this.state.players.length
+        if (this.state.gameName == null){
             return (
-                <div className={classnames('Game', className)} {...props}>
-                    <p>'... please load ...'</p>
-                </div>
+				<div className={classnames('Game', className)} {...props}>
+					<form onSubmit={this.handleGameLoad}>
+						<label>
+							<input type="text" onChange={this.handleGameName} />
+				        </label>
+				    <input type="submit" value="Submit" />
+				    </form>
+				</div>
             )
         }
-        var players = this.props.getPlayers(this.props.gameName)
         return (
             <div className={classnames('Game', className)} {...props}>
-            <p>{this.state.players}.</p>
+            <p>{numPlayers} players</p>
             </div>
         );
     }
