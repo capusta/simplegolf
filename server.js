02a28c78cfbe9f2091f9cf6e6a2cfb5e104ffd19
capusta <gvozdev@gmail.com>
2017-04-09 00:55:25 -0700

    Something about openshift needing this

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var ip      = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 app.use(bodyParser.json());
 // Express Router Stuff - mostly JSON calls
 app.use('/api',require('./routes/api'));
+app.use('',require('./routes/openshift'))
 
 // React stuff ... routing and static display
 app.use(express.static(path.resolve(__dirname, 'build')));
