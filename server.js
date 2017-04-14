aaa6db558b147ff90b65b89ef54ca5626f4a7fc0
capusta <gvozdev@gmail.com>
2017-04-14 00:38:53 -0700

    Umm we need this cuz we use DNS CNAMES

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use(bodyParser.json());
 // Express Router Stuff - mostly JSON calls
 app.use('/api',require('./routes/api'));
 app.use('',require('./routes/openshift'))
+app.use(function (req, res, next) {
+      res.header('Access-Control-Allow-Origin', '*');
+      next();
+})
 
 // React stuff ... routing and static display
 app.use(express.static(path.resolve(__dirname, 'build')));
